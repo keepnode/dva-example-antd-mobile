27428ec56c29f6e03b7171bf28fd5ffa4805af01
neolu <1113865828@qq.com>
2017-10-15 17:26:49 +0800

    fix: 取消轮播广告

--- src/components/ad.js
+++ src/components/ad.js
@@ -8,29 +8,23 @@ class Ad extends React.Component {
   constructor(props) {
     super(props)
     this.state={
-      data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
-      initialHeight: 200,
+      data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI']
     }
   }
 
   render(){
-    const hProp = this.state.initialHeight ? { width:'100%', height: this.state.initialHeight } : {};
-
     return (
         <Carousel autoplay={true} infinite={true} selectedIndex={0} swipeSpeed={35}
                   beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
                   afterChange={index => console.log('slide to', index)}>
           {this.state.data.map(ii => (
-            <a href="http://www.baidu.com" key={ii} style={hProp}>
+            <a href="http://www.baidu.com" key={ii}>
               <img
                 src={`https://zos.alipayobjects.com/rmsportal/${ii}.png`}
                 alt=""
                 onLoad={() => {
                   // fire window resize event to change height
                   window.dispatchEvent(new Event('resize'));
-                  this.setState({
-                    initialHeight: null,
-                  });
                 }}
                 className={style.ad_full_screen}
               />
